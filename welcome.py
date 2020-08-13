c735c9fcdcc2fc5fafdfa8fe1c62f295a1c6a0bb
emmanueltukpe <tukpeemmanuel@gmail.com>
2020-08-13 20:06:17 +0100

    Imported datetime module to get current year

---
+++ welcome.py
@@ -0,0 +1,24 @@
+import datetime
+
+
+def age_check():
+    age_str = input('How old are you?: ')
+    try:
+        y = int(age_str)
+    except:
+        print("Provide your age in numbers")
+        return age_check()
+    else:
+        return y
+
+
+first_name = input("Provide your first name: ")
+last_name = input("Provide your last name: ")
+age = age_check()
+
+now = datetime.datetime.now()
+year = now.year
+year_of_birth = year - age
+
+print(f"Hello {first_name} {last_name}",
+      f"you were born in the year {year_of_birth}")
