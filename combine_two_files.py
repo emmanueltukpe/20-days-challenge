42b6326f2462251744b97af887b3aeec0a0eab67
emmanueltukpe <tukpeemmanuel@gmail.com>
2020-08-23 16:22:03 +0100

    Added combined two files

---
+++ combine_two_files.py
@@ -0,0 +1,25 @@
+
+text_file = open("my_file.txt", "r")
+lines = text_file.readlines()
+text_file.close()
+
+other_text_file = open("my_other_file.txt", "r")
+second_lines = other_text_file.readlines()
+other_text_file.close()
+
+lines = [x.strip() for x in lines]
+second_lines = [x.strip() for x in second_lines]
+new_word= []
+
+for x,y in zip(lines, second_lines):
+    thrid_lines = (f'{x} {y}"\n')
+    new_word.append(thrid_lines)
+
+
+third_text_file= open("output_text_file.txt","w+")
+for n in new_word:
+    third_text_file.writelines(n)
+third_text_file.close()
+    
+    
+
