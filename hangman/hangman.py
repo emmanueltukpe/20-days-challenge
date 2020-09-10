552f304527f1f8e3ccbf69408934e5a399c0a077
emmanueltukpe <tukpeemmanuel@gmail.com>
2020-09-10 15:28:32 +0100

    added a dictonary to store the users correct answers

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -7,21 +7,35 @@ words = ["Antartica", "Mississippi", "Opulent", "Capricious", "Xenophobe"]
 chosen_word = random.choice(words).upper()
 
 def play_hangman(chosen_word):
-    answer = " "
+    answer = {}
+    for letter in chosen_word:
+        answer[letter] = False
     guessed = False
     guessed_letter = []
+    tries = 0
     
     while not guessed:
         print("Let's play hangman!")
         print("\n")
+        tries += 1
         guess = input("Please guess a letter: ").upper()
+        
+        if tries == 1:
+            print (f"You have made {tries} try")
+        else:
+            print (f"You have made {tries} tries")
+
         if guess == "!":
+            print(answer)
             sys.exit(0)
 
         else:
-            print("Press ! to exit game")
+            print("Press ! to exit game")           
             if len(guess) == 1 and guess.isalpha():
-                if guess not in chosen_word:
+                if guess in guessed_letter:
+                    print("You already guessed the letter", guess)
+
+                elif guess not in chosen_word:
                     print(guess, "NO")
                     guessed_letter.append(guess)
 
@@ -30,11 +44,10 @@ def play_hangman(chosen_word):
                     guessed_index = [n for n in range(
                         len(chosen_word)) if chosen_word.find(guess, n) == n]
                     print("YES", guessed_index)
-                    if " " not in answer:
-                        guessed = True
+                    answer[guess] = True
+
             else:
                 print("NO")
-            return play_hangman(chosen_word)
 
 
 print(play_hangman(chosen_word))
