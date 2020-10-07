b5d07536b58007e6bfb0de82a6c55e7cd4c6e6b8
emmanueltukpe <tukpeemmanuel@gmail.com>
2020-10-07 16:49:39 +0100

    refactored the code to use classes and objects

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -22,94 +22,103 @@ def get_target_words():
 chosen_word = get_target_words()
 
 
-
-def hangman_algorithm():
-    # the random word generated is stored in a dictionary
-    # all the characters in the word are set to false
-
-    answers = {}
-    for letter in chosen_word:
-        answers[letter] = False
-    return answers
-answers = hangman_algorithm()
-
-def check_function():
-    check_answers = {}
-    for letter in chosen_word:
-        check_answers[letter] = True
-    return check_answers
-check_answers = check_function()
-
-def display_current_guess(answer, chosen_word, guess):
-    #The function also takes correct guesses and prints out the position of each guesssed letter.
-    
-
-    word_as_list = list(answer)
-    guessed_index = [i for i, letter in enumerate(chosen_word) if letter == guess]
-    for letter in guessed_index:
-        word_as_list[letter] = guess
-    answer = "".join(word_as_list)
-    return answer
-
-def play_hangman(chosen_word):
-    # takes input from the user, enabling him to play hangman.
-    
-    answer = "_" * len(chosen_word)
-    guessed = False
-    guessed_letter = []
-    tries = 5
-    print("Let's play hangman!")
-    print("You have 5 tries, goodluck")
-    print(answer)
-    print("\n")
-
-    while not guessed and tries > 0:
-
-        guess = input("Please guess a letter: ").upper()
+class Hangman:
+    def __init__(self, chosen_word):
+        self.chosen_word = chosen_word
+        self.answers = {}
+        self.check_answers = {}
+        self.answer = "_"*len(self.chosen_word)
         
-        if guess == "!":
-            print(f"The word was {chosen_word}. Better luck next time")
-            sys.exit(0)
+        
+    def hangman_algorithm(self):
+        # the random word generated is stored in a dictionary
+        # all the characters in the word are set to false
 
-        else:
-            print("Press ! to exit game")           
-            if len(guess) == 1 and guess.isalpha():
-                if guess in guessed_letter:
-                    tries -= 1
-                    print("You already guessed the letter")
-                    if tries is 1:
-                        print (f'You have only a try left')
-                    else:
-                        print(f'You have {tries} tries left')
+        for letter in self.chosen_word:
+            self.answers[letter] = False
+        return self.answers
 
-                elif guess not in chosen_word:
-                    tries -= 1
-                    print(guess)
-                    if tries is 1:
-                        print (f'Oops you are wrong, You have only a try left')
-                    else:
-                        print(f'Oops you are wrong, You have {tries} tries left')
-                    guessed_letter.append(guess)
+    
+    def check_function(self):
+        for letter in self.chosen_word:
+            self.check_answers[letter] = True
+        return self.check_answers
 
-                else:
-                    guessed_letter.append(guess)
-                    answers[guess] = True
-                    answer = display_current_guess(answer, chosen_word, guess)
-                    if answers == check_answers:
-                        guessed = True
+
+    def display_current_guess(self):
+        #The function also takes correct guesses and prints out the position of each guesssed letter.
+        
+        
+        word_as_list = list(self.answer)
+        guessed_index = [i for i, letter in enumerate(self.chosen_word) if letter == self.guess]
+        for letter in guessed_index:
+            word_as_list[letter] = self.guess
+        self.answer = "".join(word_as_list)
+        return self.answer
+    
+
+    def play_hangman(self):
+        # takes input from the user, enabling him to play hangman.
+        
+        guessed = False
+        guessed_letter = []
+        self.answers = self.hangman_algorithm()
+        self.check_answers = self.check_function()
+        tries = 5
+        print("Let's play hangman!")
+        print("You have 5 tries, goodluck")
+        print(self.answer)
+        print("\n")
+
+        while not guessed and tries > 0:
+
+            self.guess = input("Please guess a letter: ").upper()               
+            if self.guess == "!":
+                print(f"The word was {self.chosen_word}. Better luck next time")
+                sys.exit(0)
 
             else:
-                tries -= 1
-                if tries is 1:
-                    print (f'Not valid, enter an actual letter!, You have only a try left')
+                print("Press ! to exit game")           
+                if len(self.guess) == 1 and self.guess.isalpha():
+                    if self.guess in guessed_letter:
+                        tries -= 1
+                        print("You already guessed the letter")
+                        if tries is 1:
+                            print (f'You have only a try left')
+                        else:
+                            print(f'You have {tries} tries left')
+
+                    elif self.guess not in self.chosen_word:
+                        tries -= 1
+                        print(self.guess)
+                        if tries is 1:
+                            print (f'Oops you are wrong, You have only a try left')
+                        else:
+                            print(f'Oops you are wrong, You have {tries} tries left')
+                        guessed_letter.append(self.guess)
+
+                    else:
+                        guessed_letter.append(self.guess)
+                        self.answers[self.guess] = True
+                        self.answer = self.display_current_guess()
+                        if self.answers == self.check_answers:
+                            guessed = True
+
                 else:
-                    print(f"Not valid, enter an actual letter! You have {tries} tries left")
+                    tries -= 1
+                    if tries is 1:
+                        print (f'Not valid, enter an actual letter!, You have only a try left')
+                    else:
+                        print(f"Not valid, enter an actual letter! You have {tries} tries left")
 
-        print(answer)
-        print("\n")    
-    if guessed:
-         print("Congrats, you've guessed the word! You won!")
-    else:
-        print("Sorry, you've run out of tries. The word was " + chosen_word + ". Maybe next time!")
+            print(self.answer)
+            print("\n")
+            
+        if guessed:
+            print("Congrats, you've guessed the word! You won!")
+        else:
+            print("Sorry, you've run out of tries. The word was " + chosen_word + ". Maybe next time!")
 
-play_hangman(chosen_word)
+chosen_word = get_target_words()
+hangman = Hangman(chosen_word)
+hangman.play_hangman()
